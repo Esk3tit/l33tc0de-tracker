d0c7e262b422b4abb82acc2a91c3adbcc7e2bea2
Khai Phan <khaiphn41@gmail.com>
2023-07-24 05:35:08 -0700

    Moved components and atoms folder outside of app directory and started work on auth modals for signup/sign in/forgot password.

---
+++ src/components/Modals/Login.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+type LoginProps = {
+    
+};
+
+const Login:React.FC<LoginProps> = () => {
+    
+    return <form className='space-y-6 px-6 pb-4'>
+        <h3 className='text-xl font-medium text-white'>Sign in to L33tc0de Tracker</h3>
+        <div>
+            <label htmlFor='email' className='text-sm font-medium block mb-2 text-gray-300'>
+                Email address
+            </label>
+            <input
+                id='email'
+                name='email'
+                type='email'
+                autoComplete='email'
+                required
+                className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+                    bg-gray-600 border-gray-500 placeholder-gray-400 text-white'
+                placeholder='name@domain.com'
+            />
+        </div>
+
+        <div>
+            <label htmlFor='password' className='text-sm font-medium block mb-2 text-gray-300'>
+                Password
+            </label>
+            <input
+                id='password'
+                name='password'
+                type='password'
+                autoComplete='password'
+                required
+                className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+                    bg-gray-600 border-gray-500 placeholder-gray-400 text-white'
+            />
+        </div>
+
+        <button type='submit' className='w-full text-white focus:ring-blue-300 font-medium rounded-lg
+        text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s'>
+            Sign in
+        </button>
+        <button type='button' className='flex w-full justify-end'>
+            <a href='#' className='text-sm block text-brand-orange hover:underline w-full text-right'>
+                Forgot password?
+            </a>
+        </button>
+        <div className='text-sm font-medium text-gray-300'>
+            Don't have an account?{' '}
+            <a href='#' className='text-blue-700 hover:underline'>
+                Create account
+            </a>
+        </div>
+    </form>
+}
+export default Login;
