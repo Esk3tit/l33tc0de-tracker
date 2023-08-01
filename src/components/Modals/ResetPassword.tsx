aa4b2dce2519522f39cb091fc4fb29ea0181431e
Khai Phan <khaiphn41@gmail.com>
2023-08-01 05:48:20 -0700

    Added problems list page with navbar.
    Also added logout and reset password functionality as well.

--- src/components/Modals/ResetPassword.tsx
+++ src/components/Modals/ResetPassword.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { auth } from '@/firebase/firebase';
+import React, { useEffect, useState } from 'react';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
 
 type ResetPasswordProps = {
 
@@ -6,8 +8,25 @@ type ResetPasswordProps = {
 
 const ResetPassword: React.FC<ResetPasswordProps> = () => {
 
+    const [email, setEmail] = useState('');
+    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
+    const handleReset = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!email) return alert('Please fill in all fields');
+        try {
+            const success = await sendPasswordResetEmail(email);
+            if (success) alert('Password reset email sent');
+        } catch(err: any) {
+            alert(err.message);
+        }
+    };
+
+    useEffect(() => {
+        if (error) alert(error.message);
+    }, [error]);
+
     return (
-        <form className='space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8'>
+        <form className='space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8' onSubmit={handleReset}>
             <h3 className='text-xl font-medium  text-white'>Reset Password</h3>
             <p className='text-sm text-white '>
                 Forgotten your password? Enter your e-mail address below, and we&apos;ll send you an e-mail allowing you
@@ -21,6 +40,7 @@ const ResetPassword: React.FC<ResetPasswordProps> = () => {
                     type='email'
                     name='email'
                     id='email'
+                    onChange={(e) => setEmail(e.target.value)}
                     className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white'
                     placeholder='name@domain.com'
                 />
