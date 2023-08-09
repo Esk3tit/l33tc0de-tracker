3020159d6ab1ac499fa70ebfa5e3cf806e9e957b
Khai Phan <khaiphn41@gmail.com>
2023-08-08 23:23:36 -0700

    Finished hardcoded version of the workspace with problem description, code editor, test cases, and footer.
    Hard coded right now so will need to make dynamic later
    
    navbar settings added too I guess but will also need to be modified later.

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Split from 'react-split';
 import ProblemDescription from './ProblemDescription/ProblemDescription';
+import Playground from './Playground/Playground';
 
 type WorkspaceProps = {
     
@@ -10,9 +11,9 @@ type WorkspaceProps = {
 
 const Workspace:React.FC<WorkspaceProps> = () => {
     
-    return <Split className='split'>
+    return <Split className='split' minSize={0}>
         <ProblemDescription />
-        <div>code editor</div>
+        <Playground />
     </Split>
 }
 export default Workspace;
