eca6178c8dc94317948c05b192561cc0ace7174c
Khai Phan <khaiphn41@gmail.com>
2023-08-15 04:59:24 -0700

    Added way of adding problems to DB thru temp form. Might make this as part of an admin web panel to add problems to the site in prod.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,46 @@
+'use client'
+
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
+import { firestore } from "@/firebase/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import React, { useState } from "react";
 
 export default function Home() {
+
+  const [inputs, setInputs] = useState({
+    id: "",
+    title: "",
+    difficulty: "",
+    category: "",
+    videoId: "",
+    link: "",
+    order: 0,
+    likes: 0,
+    dislikes: 0
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value
+    })
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newInputs = {
+      ...inputs,
+      order: Number(inputs.order)
+    }
+
+    await setDoc(doc(firestore, "problems", inputs.id), newInputs);
+    alert("Document successfully written!");
+  };
+
+  console.log(inputs);
+
   return (
     <main className="bg-dark-layer-2 min-h-screen">
       <Topbar />
@@ -35,6 +74,18 @@ export default function Home() {
           <ProblemsTable />
         </table>
       </div>
+
+      {/* TEMP FORM */}
+      <form className="p-6 flex flex-col max-w-sm gap-3" onSubmit={handleSubmit}>
+        <input onChange={handleInputChange} type="text" placeholder="problem id" name="id" />
+        <input onChange={handleInputChange} type="text" placeholder="title" name="title" />
+        <input onChange={handleInputChange} type="text" placeholder="difficulty" name="difficulty" />
+        <input onChange={handleInputChange} type="text" placeholder="category" name="category" />
+        <input onChange={handleInputChange} type="text" placeholder="videoId?" name="videoId" />
+        <input onChange={handleInputChange} type="text" placeholder="link?" name="link" />
+        <input onChange={handleInputChange} type="text" placeholder="order" name="order" />
+        <button type="submit" className="bg-white">Submit to Firestore DB</button>
+      </form>
     </main>
   )
 }
