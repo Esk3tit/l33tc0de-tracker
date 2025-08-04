92599a133736131ecd1219e1c681e0ca0a87a3fb
Khai Phan <khaiphn41@gmail.com>
2025-08-03 23:20:24 -0700

    Remove auth page and fix static generation prod

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone', // Good for Fly.io
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  // Skip static generation for these pages
+  async generateStaticParams() {
+    return []
+  }
+}
 
 module.exports = nextConfig
